2444977f6014813f1aefe060c7892160d6c35e25
dindha maria <dhiendhamaria@rocketmail.com>
2015-10-01 14:54:09 +0700

    Dindha Maria
    Tugas Pengenalan Properties & Event

---
+++ MI-2D-1431140045/MI-2D-1431140045/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Purple Then
+            Me.BackColor = Color.AntiqueWhite
+        Else
+            Me.BackColor = Color.Purple
+        End If
+    End Sub
+End Class
